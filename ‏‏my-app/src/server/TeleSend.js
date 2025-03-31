3baf79d9b6a81f32d57f91c21878fe53e9862c42
Aabwshmys <aabwshmys84@gmail.com>
2025-03-31 06:00:38 +0300

    Update TeleSend.js

--- ‏‏my-app/src/server/TeleSend.js
+++ ‏‏my-app/src/server/TeleSend.js
@@ -16,7 +16,7 @@ export const TeleSned = () => {
         //    },
         //   ],
         const text = des
-        await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}text=${text}`)
+        await axios.post(`https://api.telegram.org/bot${process.env.TOKEN}/sendMessage?chat_id=${process.env.CHAT_ID}text=mm`)
 
     }
   return {
