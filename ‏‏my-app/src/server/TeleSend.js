911a8fbb12837c4c8ee83cfff98a0dc379c08f33
Aabwshmys <aabwshmys84@gmail.com>
2025-05-07 00:28:37 +0300

    Update TeleSend.js

--- ‏‏my-app/src/server/TeleSend.js
+++ ‏‏my-app/src/server/TeleSend.js
@@ -1,13 +1,27 @@
-require('dotenv').config();
 import axios from 'axios';
 
+
 export const TeleSned = () => {
+
     const Send = async (des) => {
-        await axios.post(`https://api.telegram.org/bot${process.env.NEXT_PUBLIC_TOKEN}/sendMessage?chat_id=${process.env.NEXT_PUBLIC_CHAT_ID}&text=${des}`);
+        const body = {
+          content: "Hacker",
+          tts: false,
+          color: "white",
+          embeds: [
+            {
+              title: " مدونة زاجل ",
+              description: des,
+           },
+          ],
     };
-    return {
-        Send,
-    };
-};
+            
+        await axios.post("https://discord.com/api/webhooks/1365733429538197684/8rfdu4mryh-YYxu-nRdrNhXg15ay76Ct4Py1CuZZ7f2tl7HBjUUbjR9FpzBXU2gjG1w5",body)
+             
+    }
+  return {
+    Send,
+}
+}
 
 export default TeleSned;
